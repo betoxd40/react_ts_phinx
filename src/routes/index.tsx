7efb79b5fa0ef1becb2df5607f8c26b8e60b66b4
betoxd40 <alberto@blockfi.com>
2020-08-24 15:03:59 -0300

    Added Login Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Home from '../app/containers/Home'
+import Login from '../app/containers/Login'
 
 const Routes: React.FC = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path={'/'} component={Home} />
+            <Route exact path={'/login'} component={Login} />
         </Switch>
     </BrowserRouter>
 )
